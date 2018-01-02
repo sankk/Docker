8445a1f03a75587889fccda530ed44a96f542ce9
gino <gino@phoenix-5.com>
2018-01-02 13:39:40 +0800

    up nginx images version 1.13.5

--- md.sh
+++ md.sh
@@ -6,11 +6,11 @@ mkdir -p /docker-mount/node8/app
 mkdir -p /docker-mount/nginx/logs && mkdir -p /docker-mount/nginx/conf && mkdir -p /docker-mount/nginx/conf/cert
 mkdir -p /docker-mount/php && mkdir -p /docker-mount/php/logs
 mkdir -p /docker-mount/cron && touch /docker-mount/cron/cron.log
-groupadd -g 33 www-data && useradd -s /sbin/nologin -d /home/www-data www-data -g www-data
+#groupadd -g 33 www-data && useradd -s /sbin/nologin -d /home/www-data www-data -g www-data
 cp ./php/php.ini /docker-mount/php
 cp ./devops/nginx/nginx.conf /docker-mount/nginx/
 cp ./devops/nginx/cert/* /docker-mount/nginx/conf/cert/
 cp ./devops/nginx/conf.d/* /docker-mount/nginx/conf/
 cp ./devops/node8/app/* /docker-mount/node8/app/
 cp 0schema.sql /docker-mount/mariadb/docker-entrypoint-initdb.d/
-chown -R www-data:www-data /docker-mount/www
+#chown -R www-data:www-data /docker-mount/www
